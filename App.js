72ed5537267f479c95705de5632c72d535ee8c9d
Stanley Chan <nahcnats@gmail.com>
2021-02-26 18:23:56 +0800

    home screen

---
+++ App.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import SplashScreen from 'react-native-splash-screen';
+
+// impport navigation
+import Tabs from './navigation/tabs';
+
+// import screens
+import { CryptoDetail, Transaction } from "./screens";
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+        initialRouteName={'Home'}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Tabs}
+        />
+        <Stack.Screen
+          name="CryptoDetail"
+          component={CryptoDetail}
+        />
+        <Stack.Screen
+          name="Transaction"
+          component={Transaction}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
